73c7f21ad5eb26f96cdd7bb9713ee99988e88d46
norbey095 <norbey095@gmail.com>
2024-08-15 10:19:35 -0500

    Base del microservicio stock

---
+++ src/main/java/com/emazon/api_stock/ApiStockApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.api_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStockApplication.class, args);
+	}
+
+}
