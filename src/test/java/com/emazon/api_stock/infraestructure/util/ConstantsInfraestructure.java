b08dc2214616b16351c3f7f8347de7b831e2e5a3
norbey095 <norbey095@gmail.com>
2024-10-12 14:00:46 -0500

    Mensajes faltantes a español

--- src/test/java/com/emazon/api_stock/infraestructure/util/ConstantsInfraestructure.java
+++ src/test/java/com/emazon/api_stock/infraestructure/util/ConstantsInfraestructure.java
@@ -14,7 +14,7 @@ public class ConstantsInfraestructure {
     public static final String JSON_BRAND_REQUEST = "{\"name\": \"Electronics\", \"description\":" +
             " \"brand for electronics\"}";
     public static final String CATEGORY_EXISTS = "Ya existe una categoría con ese nombre";
-    public static final String BRAND_EXISTS = "There is already a brand with that name";
+    public static final String BRAND_EXISTS = "Ya existe una marca con ese nombre";
     public static final String INVALID_NAME = "Invalid argument";
     public static final String INVALID_DESCRIPTION = "Invalid argument";
     public static final String NO_DATA_FOUND_EXCEPTION_MESSAGE = "No se encontraron datos";
@@ -29,20 +29,15 @@ public class ConstantsInfraestructure {
     public static final String FIELD_CATEGORIES_NULL="The 'categories' field cannot be empty.";
     public static final String REPEATED_CATEGORIES="Categories cannot be repeated";
     public static final String FIELD_ARTICLES_DESCRIPTION="articles for electronics";
-    public static final String ARTICLE_EXISTS="There is already an article with that name";
+    public static final String ARTICLE_EXISTS="Ya existe un artículo con ese nombre";
     public static final String MESSAGESS = "$.message";
-    public static final String MESSAGESS_NAME = "$[0].name";
-    public static final String MESSAGESS_DESCRIPTION = "$[0].description";
-    public static final String MESSAGESS_QUANTITY = "$[0].quantity";
     public static final String PAGE = "page";
     public static final String SIZE = "size";
     public static final String VALUE_UNO = "1";
-    public static final String VALUE_DOS = "2";
     public static final Integer VALUE_0 = 0;
     public static final Integer VALUE_1 = 1;
     public static final Integer VALUE_2 = 2;
     public static final Integer VALUE_3 = 3;
-    public static final String TRUE = "true";
     public static final boolean VALUE_FALSE = false;
     public static final String ARTICLE = "article";
     public static final String COMILLAS = "";
